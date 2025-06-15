33c111751d159dc91c3bca3916de70fca545b5e1
Aashif M <Aashif8056@gmail.com>
2025-06-15 11:36:45 +0530

    Update setup-fw.sh

--- setup-fw.sh
+++ setup-fw.sh
@@ -2,4 +2,4 @@
 echo "[*] Setting iptables rules..."
 sudo iptables -I INPUT -j NFQUEUE --queue-num 1
 sudo iptables -I OUTPUT -j NFQUEUE --queue-num 1
-echo "[*] Done. Run stealth_firewall.py with sudo."
+echo "[*] Done. Run firewall.py with sudo."
